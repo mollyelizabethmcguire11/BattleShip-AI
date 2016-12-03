e0fad2f962b1d7ac8b009d8c82a460c3aa6041e2
Jeremy Daigneau <jdaigneau5@gmail.com>
2016-12-03 17:47:22 -0500

    added getRemainingActions and updateQValue

--- Agent.py
+++ Agent.py
@@ -26,116 +26,116 @@ DESTROYER = 2
 def Agent:
 
 	#initialize agent. Arguments to be determined...
-	def __init__(self):
+	def __init__(self, alpha=0.3, gamma=0.9, epsilon=0.2):
 
-	#return if a given shot hits or misses
-	def isHit(self, coords):
+		#learning rate
+		self.alpha = float(alpha)
 
-		#logic to be determined...
-		return true
+		#discount factor
+		self.discount = float(gamma)
 
-	#random shot function. Used for first and second strategy.
+		#epsilon
+		self.epsilon = float(epsilon)
 
-	def shoot(self, xcoord, ycoord):
-		#this would integrate with the actual game to take the shot
-		#return true if hit was made
-	def randomShot(self):
+		self.qValues = util.Counter()
+		
+		#reward for hitting a target
+		#self.hitReward = 1
 
-		#this is the general idea. Integration with the actual battleship game will let us test
-		#when this function is called, the return value should be checked against already target locations
-		xcoord = randit(0, WIDTH - 1)
-		ycoord = randit(0, HEIGHT - 1)
+		#Penalty for missing
+		#self.missReward = -.5
 
-		return (xcoord, ycoord)
+		#All actions taken
+		self.shotsFired = []
+
+	
+	def getRemainingActions(self, shotsFired):
+
+		remainingActions = []
+
+		for i in range(WIDTH):
+			for j in range(HEIGHT):
+				if shotsFired[i][j] != None:
+					remainingActions.append((i,j))
+
+		return remainingActions
 
 	#function for hunt/target algorithm. Used for second strategy.
-	def huntTarget(self):
+	# def huntTarget(self):
 
-		#stack of all shots fired
-		oldCoords = []
+	# 	#stack of all shots fired
+	# 	oldCoords = []
 
-		#Will take random shots until a hit is made
+	# 	#Will take random shots until a hit is made
 
-		target = randomShot()
-		oldCoords.push(target)
+	# 	target = randomShot()
+	# 	oldCoords.push(target)
 
-		while shoot(target) != True and target not in oldCoords:
+	# 	while shoot(target) != True and target not in oldCoords:
 
-			target = randomShot()
+	# 		target = randomShot()
 
-			if target not in oldCoords:
-				oldCoords.push(target)
+	# 		if target not in oldCoords:
+	# 			oldCoords.push(target)
 
-		#Next we would check the nearby coordinates after we land a hit
-		currentTarget = target
+	# 	#Next we would check the nearby coordinates after we land a hit
+	# 	currentTarget = target
 
-		while shoot(target) == True:
+	# 	while shoot(target) == True:
 			
-			temp = target
+	# 		temp = target
 
-			target.ycoord = target.ycoord + 1
+	# 		target.ycoord = target.ycoord + 1
 			
-			if target not in oldCoords:
-				oldCoords.push(target)
+	# 		if target not in oldCoords:
+	# 			oldCoords.push(target)
 
-			else:
-				target = temp
+	# 		else:
+	# 			target = temp
 
-		target = currentTarget	
+	# 	target = currentTarget	
 
-		while shoot(target) == True:
+	# 	while shoot(target) == True:
 
-			temp = target
+	# 		temp = target
 
-			target.ycoord = target.ycoord - 1
+	# 		target.ycoord = target.ycoord - 1
 
-			if target not in oldCoords:
-				oldCoords.push(target)
+	# 		if target not in oldCoords:
+	# 			oldCoords.push(target)
 
-			else:
-				target = temp
+	# 		else:
+	# 			target = temp
 
-		target = currentTarget	
+	# 	target = currentTarget	
 
 
-		while shoot(target) == True:
+	# 	while shoot(target) == True:
 
-			temp = target
+	# 		temp = target
 
-			target.xcoord = target.xcoord + 1
+	# 		target.xcoord = target.xcoord + 1
 
-			if target not in oldCoords:
-				oldCoords.push(target)
+	# 		if target not in oldCoords:
+	# 			oldCoords.push(target)
 
-			else:
-				target = temp
+	# 		else:
+	# 			target = temp
 
-		target = currentTarget	
+	# 	target = currentTarget	
 
-		while shoot(target) == True:
+	# 	while shoot(target) == True:
 
-			temp = target 
+	# 		temp = target 
 
-			target.xcoord = target.xcoord - 1
+	# 		target.xcoord = target.xcoord - 1
 			
-			if target not in oldCoords:
-				oldCoords.push(target)
+	# 		if target not in oldCoords:
+	# 			oldCoords.push(target)
 
-			else:
-				target = temp
+	# 		else:
+	# 			target = temp
 
-
-
-
-	#the best algorithim of the three, using Q-learning and prob reasoning
-	def ProbReasoning(self):
-
-	#other functions to create
-
-	#updates state of board after a move is made
-	def updateBoard(self):
-
-	#Q-learning functions
 
 	#computes the best action based on the state and current qValue
 	def computeActionsFromQValues(self):
@@ -143,7 +143,7 @@ def Agent:
 
 	def getQValue(self):
 
-	def update_qvalue(self,position, isHit,distance,counter):
+	def update_qvalue(self,isHit,target,counter):
 
 		#Code based on code from the Berkley Pacman projects
         """
@@ -153,6 +153,8 @@ def Agent:
           NOTE: You should never call this function,
           it will be called on your behalf
         """
+        legalActions = self.getRemainingActions(self.shotsFired)
+
         if counter == 0:
             reward = 0
 
@@ -163,11 +165,14 @@ def Agent:
         
         #Function for Q value iteration    
         #sample = reward + self.discount * max(self.getQValue() 
-        if isHit:
-            self.qValuesOfhit[distance] = (1-self.alpha) * self.getQValue(distance,isHit) + self.alpha * sample
-        else:
-            self.qValuesOfMiss[distance] = (1-self.alpha) * self.getQValue(distance,isHit) + self.alpha * sample
+        #if isHit:
 
-	def getAction(self):
+        sample = reward + self.discount * max(self.getQValue(action,hitOrnot) for action in legalActions)
+        self.qValues[target] = (1-self.alpha) * self.getQValue(target,isHit) + self.alpha * sample
+
+        #else:
+         #   self.qValuesOfMiss[distance] = (1-self.alpha) * self.getQValue(distance,isHit) + self.alpha * sample
+
+	#def getAction(self):
 
 
