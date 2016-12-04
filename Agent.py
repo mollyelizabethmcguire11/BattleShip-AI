9be0332f331506600311158d38e2e2a76740fcb4
Jeremy Daigneau <jdaigneau5@gmail.com>
2016-12-04 14:01:25 -0500

    in test-agent.py added code to agent class followed by separate main function

--- Agent.py
+++ Agent.py
@@ -1,17 +1,17 @@
 # This will be where the logic for the Battleship agent will be.
 # Currently, our goal is to create three different playing styles for the agent
 #
-# 	1. Random Shooting - the agent selects a target randomly
+#   1. Random Shooting - the agent selects a target randomly
 #
-#	2. Hunt/Target strategy - The agent begins by shooting randomly, hunting a ship.
-#	   when a ship is hit, the agent begins targeting that ship by searching the locations near the hit.
+#   2. Hunt/Target strategy - The agent begins by shooting randomly, hunting a ship.
+#      when a ship is hit, the agent begins targeting that ship by searching the locations near the hit.
 #
-#	3. Probabalistic Reasoning - Aim for the most probable locations. This will determine where a ship can fit
-#	   on the board based on the current state, given the board size and the number of shots taken.
+#   3. Probabalistic Reasoning - Aim for the most probable locations. This will determine where a ship can fit
+#      on the board based on the current state, given the board size and the number of shots taken.
 #
 # This logic is based off information found at http://www.datagenetics.com/blog/december32011/
 
-import random, util.py
+import random, util
 
 #Globals to be used
 HEIGHT = 10
@@ -25,52 +25,52 @@ DESTROYER = 2
 
 class Agent:
 
-	#initialize agent. Arguments to be determined...
-	def __init__(self, alpha=0.3, gamma=0.9, epsilon=0.2):
+    #initialize agent. Arguments to be determined...
+    def __init__(self, alpha=0.3, gamma=0.9, epsilon=0.2):
 
-		#learning rate
-		self.alpha = float(alpha)
+        #learning rate
+        self.alpha = float(alpha)
 
-		#discount factor
-		self.discount = float(gamma)
+        #discount factor
+        self.discount = float(gamma)
 
-		#epsilon
-		self.epsilon = float(epsilon)
+        #epsilon
+        self.epsilon = float(epsilon)
 
-		self.qValues = util.Counter()
+        self.qValues = util.Counter()
 
-		#flag to check if last shot was a hit
-		self.isHit = False
+        #flag to check if last shot was a hit
+        self.isHit = False
 
-		#reward for hitting a target
-		#self.hitReward = 1
+        #reward for hitting a target
+        #self.hitReward = 1
 
-		#Penalty for missing
-		#self.missReward = -.5
+        #Penalty for missing
+        #self.missReward = -.5
 
-		#All actions taken
-		self.shotsFired = []
-		self.hits = []
-	
-	def getRemainingActions(self, shotsFired):
+        #All actions taken
+        self.shotsFired = []
+        self.hits = []
+    
+    def getRemainingActions(self, shotsFired):
 
-		remainingActions = []
+        remainingActions = []
 
-		for i in range(WIDTH):
-			for j in range(HEIGHT):
-				if shotsFired[i][j] != None:
-					remainingActions.append((i,j))
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                if shotsFired[i][j] != None:
+                    remainingActions.append((i,j))
 
-		return remainingActions
+        return remainingActions
 
-	def computeActionsFromQValues(self, shotsFired, hits):
+    def computeActionsFromQValues(self, shotsFired, hits):
 
-		actions = self.getRemainingActions(shotsFired)
-		actValPair = util.Counter()
+        actions = self.getRemainingActions(shotsFired)
+        actValPair = util.Counter()
         qVals = []
        
         if len(actions) == 0:
-        	  
+              
           return None
 
         max_actions = []
@@ -95,36 +95,28 @@ class Agent:
 
         return max_action
 
-	def getQValue(self, action, shotsFired, hits):
+    def getQValue(self, action, shotsFired, hits):
 
-		return self.qValues[action, hits, shotsFired]
+        return self.qValues[(action, hits, shotsFired)]
 
-	def update_qvalue(self,isHit,target,counter):
+    def update_qvalue(self,isHit,target,counter):
 
-		#Code based on code from the Berkley Pacman projects
-    	"""
-          The parent class calls this to observe a
-          state = action => nextState and reward transition.
-          You should do your Q-Value update here
-          NOTE: You should never call this function,
-          it will be called on your behalf
-        """
         legalActions = self.getRemainingActions(self.shotsFired)
 
         if counter == 0:
-        	reward = 0
+            reward = 0
 
         elif isHit:
             reward=0.5
         else:
             reward=0.1
         
-        sample = reward + self.discount * max(self.getQValue(action,hitOrnot) for action in legalActions)
-        self.qValues[target] = (1-self.alpha) * self.getQValue(target,isHit) + self.alpha * sample
+        sample = reward + self.discount * max(self.getQValue(action,isHit) for action in legalActions)
+        self.qValues[(action,self.hits, self.shotsFired)] = (1-self.alpha) * self.getQValue(action,isHit) + self.alpha * sample
 
         #else:
          #   self.qValuesOfMiss[distance] = (1-self.alpha) * self.getQValue(distance,isHit) + self.alpha * sample
 
-	#def getAction(self):
+    #def getAction(self):
 
 
--- test-agent.py
+++ test-agent.py
@@ -1,5 +1,5 @@
 
-import random, util.py, copy
+import random, util, copy
 
 #Globals to be used
 HEIGHT = 10
@@ -13,48 +13,48 @@ DESTROYER = 2
 
 class Agent:
 
-	#initialize agent. Arguments to be determined...
-	def __init__(self, alpha=0.3, gamma=0.9, epsilon=0.2):
+    #initialize agent. Arguments to be determined...
+    def __init__(self, alpha=0.3, gamma=0.9, epsilon=0.2):
 
-		#learning rate
-		self.alpha = float(alpha)
+        #learning rate
+        self.alpha = float(alpha)
 
-		#discount factor
-		self.discount = float(gamma)
+        #discount factor
+        self.discount = float(gamma)
 
-		#epsilon
-		self.epsilon = float(epsilon)
+        #epsilon
+        self.epsilon = float(epsilon)
 
-		self.qValues = util.Counter()
+        self.qValues = util.Counter()
 
-		#flag to check if last shot was a hit
-		self.isHit = False
+        #flag to check if last shot was a hit
+        self.isHit = False
 
-		#reward for hitting a target
-		#self.hitReward = 1
+        #reward for hitting a target
+        #self.hitReward = 1
 
-		#Penalty for missing
-		#self.missReward = -.5
+        #Penalty for missing
+        #self.missReward = -.5
 
-		#All actions taken
-		self.shotsFired = []
-		self.hits = []
+        #All actions taken
+        self.shotsFired = []
+        self.hits = []
 
-	def getRemainingActions(self, shotsFired):
+    def getRemainingActions(self, shotsFired):
 
-		remainingActions = []
+        remainingActions = []
 
-		for i in range(WIDTH):
-			for j in range(HEIGHT):
-				if shotsFired[i][j] != None:
-					remainingActions.append((i,j))
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                if shotsFired[i][j] != None:
+                    remainingActions.append((i,j))
 
-		return remainingActions
+        return remainingActions
 
-	def computeActionsFromQValues(self, shotsFired, hits):
+    def computeActionsFromQValues(self, shotsFired, hits):
 
-	    actions = self.getRemainingActions(shotsFired)
-	    actValPair = util.Counter()
+        actions = self.getRemainingActions(shotsFired)
+        actValPair = util.Counter()
         qVals = []
 
         if len(actions) == 0:
@@ -81,21 +81,21 @@ class Agent:
 
         return max_action
 
-	def getQValue(self, action, shotsFired, hits):
+    def getQValue(self, action, shotsFired, hits):
 
-		return self.qValues[action, hits, shotsFired]
+        return self.qValues[action, hits, shotsFired]
 
-	def update_qvalue(self, isHit, target, counter):
+    def update_qvalue(self, isHit, target, counter, shotsFired):
 
-        legalActions = self.getRemainingActions(self.shotsFired)
+        legalActions = self.getRemainingActions(shotsFired)
 
         if counter == 0:
             reward = 0
 
         elif isHit:
-                reward=0.5
+            reward=0.5
         else:
-                reward=0.1
+            reward=0.1
 
         sample = reward + self.discount * max(self.getQValue(action,hitOrnot) for action in legalActions)
         self.qValues[target] = (1-self.alpha) * self.getQValue(target,isHit) + self.alpha * sample
@@ -103,249 +103,308 @@ class Agent:
         #else:
          #   self.qValuesOfMiss[distance] = (1-self.alpha) * self.getQValue(distance,isHit) + self.alpha * sample
 
-	#def getAction(self):
-	def print_board(s,board):
+    #def getAction(self):
+    def print_board(self, s,board):
 
-		# WARNING: This function was crafted with a lot of attention. Please be aware that any
-		#          modifications to this function will result in a poor output of the board
-		#          layout. You have been warn.
+        # WARNING: This function was crafted with a lot of attention. Please be aware that any
+        #          modifications to this function will result in a poor output of the board
+        #          layout. You have been warn.
 
-		#find out if you are printing the computer or user board
-		player = "Computer"
-		if s == "u":
-			player = "User"
+        #find out if you are printing the computer or user board
+        player = "Computer"
+        if s == "u":
+            player = "User"
 
-		print "The " + player + "'s board look like this: \n"
+        print "The " + player + "'s board look like this: \n"
 
-		#print the horizontal numbers
-		print " ",
-		for i in range(10):
-			print "  " + str(i+1) + "  ",
-		print "\n"
+        #print the horizontal numbers
+        print " ",
+        for i in range(10):
+            print "  " + str(i+1) + "  ",
+        print "\n"
 
-		for i in range(10):
+        for i in range(10):
 
-			#print the vertical line number
-			if i != 9:
-				print str(i+1) + "  ",
-			else:
-				print str(i+1) + " ",
+            #print the vertical line number
+            if i != 9:
+                print str(i+1) + "  ",
+            else:
+                print str(i+1) + " ",
 
-			#print the board values, and cell dividers
-			for j in range(10):
-				if board[i][j] == -1:
-					print ' ',
-				elif s == "u":
-					print board[i][j],
-				elif s == "c":
-					if board[i][j] == ('\x1b[0;31;40m' + "*" + '\x1b[0m') or board[i][j] == ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
-						print board[i][j],
-					else:
-						print " ",
+            #print the board values, and cell dividers
+            for j in range(10):
+                if board[i][j] == -1:
+                    print ' ',
+                elif s == "u":
+                    print board[i][j],
+                elif s == "c":
+                    if board[i][j] == ('\x1b[0;31;40m' + "*" + '\x1b[0m') or board[i][j] == ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
+                        print board[i][j],
+                    else:
+                        print " ",
 
-				if j != 9:
-					print ('\x1b[1;37;44m' + " | " + '\x1b[0m'),
-			print
+                if j != 9:
+                    print ('\x1b[1;37;44m' + " | " + '\x1b[0m'),
+            print
 
-			#print a horizontal line
-			if i != 9:
-				print ('\x1b[1;37;44m' + "   ----------------------------------------------------------" + '\x1b[0m')
-			else:
-				print
-
-
-	def computer_place_ships(board,ships):
-
-		for ship in ships.keys():
-
-			#genreate random coordinates and vlidate the postion
-			valid = False
-			while(not valid):
-
-				x = random.randint(1,10)-1
-				y = random.randint(1,10)-1
-				o = random.randint(0,1)
-				if o == 0:
-					ori = "v"
-				else:
-					ori = "h"
-				valid = validate(board,ships[ship],x,y,ori)
-
-			#place the ship
-			print "Computer placing a/an " + ship
-			board = place_ship(board,ships[ship],ship[0],ori,x,y)
-
-		return board
+            #print a horizontal line
+            if i != 9:
+                print ('\x1b[1;37;44m' + "   ----------------------------------------------------------" + '\x1b[0m')
+            else:
+                print
 
 
-	def place_ship(board,ship,s,ori,x,y):
+    def computer_place_ships(self,board,ships):
 
-		#place ship based on orientation
-		if ori == "v":
-			for i in range(ship):
-				board[x+i][y] = s
-		elif ori == "h":
-			for i in range(ship):
-				board[x][y+i] = s
+        for ship in ships.keys():
 
-		return board
+            #genreate random coordinates and vlidate the postion
+            valid = False
+            while(not valid):
 
-	def validate(board,ship,x,y,ori):
+                x = random.randint(1,10)-1
+                y = random.randint(1,10)-1
+                o = random.randint(0,1)
+                if o == 0:
+                    ori = "v"
+                else:
+                    ori = "h"
+                valid = self.validate(board,ships[ship],x,y,ori)
 
-		#validate the ship can be placed at given coordinates
-		if ori == "v" and x+ship > 10:
-			return False
-		elif ori == "h" and y+ship > 10:
-			return False
-		else:
-			if ori == "v":
-				for i in range(ship):
-					if board[x+i][y] != -1:
-						return False
-			elif ori == "h":
-				for i in range(ship):
-					if board[x][y+i] != -1:
-						return False
+            #place the ship
+            print "Computer placing a/an " + ship
+            board = self.place_ship(board,ships[ship],ship[0],ori,x,y)
 
-		return True
-
-	def v_or_h():
-
-		#get ship orientation from user
-		while(True):
-			user_input = raw_input("vertical or horizontal (v,h) ? ")
-			if user_input == "v" or user_input == "h":
-				return user_input
-			else:
-				print "Invalid input. Please only enter v or h"
+        return board
 
 
-	def make_move(board,x,y):
+    def place_ship(self,board,ship,s,ori,x,y):
 
-		#make a move on the board and return the result, hit, miss or try again for repeat hit
-		if board[x][y] == -1:
-			return "miss"
-		elif board[x][y] == ('\x1b[0;31;40m' + "*" + '\x1b[0m') or board[x][y] == ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
-			return "try again"
-		else:
-			return "hit"
+        #place ship based on orientation
+        if ori == "v":
+            for i in range(ship):
+                board[x+i][y] = s
+        elif ori == "h":
+            for i in range(ship):
+                board[x][y+i] = s
 
+        return board
 
-	def random_mode(board):
+    def validate(self,board,ship,x,y,ori):
 
-		#generate user coordinates from the user and try to make move
-		#if move is a hit, check ship sunk and win condition
-		while(True):
-			x = random.randint(1,10)-1
-			y = random.randint(1,10)-1
-			res = make_move(board,x,y)
-			if res == "hit":
-				print "Hit at " + str(x+1) + "," + str(y+1)
-				check_sink(board,x,y)
-				board[x][y] = ('\x1b[0;32;40m' + 'X' + '\x1b[0m')
-				if check_win(board):
-					return "WIN"
-			elif res == "miss":
-				print "Sorry, " + str(x+1) + "," + str(y+1) + " is a miss."
-				board[x][y] = ('\x1b[0;31;40m' + "*" + '\x1b[0m')
+        #validate the ship can be placed at given coordinates
+        if ori == "v" and x+ship > 10:
+            return False
+        elif ori == "h" and y+ship > 10:
+            return False
+        else:
+            if ori == "v":
+                for i in range(ship):
+                    if board[x+i][y] != -1:
+                        return False
+            elif ori == "h":
+                for i in range(ship):
+                    if board[x][y+i] != -1:
+                        return False
 
-			if res != "try again":
+        return True
 
-				return board
+    def v_or_h():
 
-	def estimate_mode(board):
-
-	def check_sink(board,x,y):
-
-		#figure out what ship was hit
-		if board[x][y] == "A":
-			ship = "Aircraft Carrier"
-		elif board[x][y] == "B":
-			ship = "Battleship"
-		elif board[x][y] == "S":
-			ship = "Submarine"
-		elif board[x][y] == "D":
-			ship = "Destroyer"
-		elif board[x][y] == "P":
-			ship = "Patrol Boat"
-
-		#mark cell as hit and check if sunk
-		board[-1][ship] -= 1
-		if board[-1][ship] == 0:
-			print ship + " Sunk"
+        #get ship orientation from user
+        while(True):
+            user_input = raw_input("vertical or horizontal (v,h) ? ")
+            if user_input == "v" or user_input == "h":
+                return user_input
+            else:
+                print "Invalid input. Please only enter v or h"
 
 
-	def check_win(board):
+    def make_move(self,board,x,y):
 
-		#simple for loop to check all cells in 2d board
-		#if any cell contains a char that is not a hit or a miss return false
-		for i in range(10):
-			for j in range(10):
-				if board[i][j] != -1 and board[i][j] != ('\x1b[0;31;40m' + "*" + '\x1b[0m') and board[i][j] != ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
-					return False
-		return True
+        #make a move on the board and return the result, hit, miss or try again for repeat hit
+        if board[x][y] == -1:
+            return "miss"
+        elif board[x][y] == ('\x1b[0;31;40m' + "*" + '\x1b[0m') or board[x][y] == ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
+            return "try again"
+        else:
+            return "hit"
 
-	def main():
 
-		#types of ships
-		ships = {"Aircraft Carrier":5,
-			     "Battleship":4,
-	 		     "Submarine":3,
-			     "Destroyer":3,
-			     "Patrol Boat":2}
+    def random_mode(self,board):
 
-		#setup blank 10x10 board
-		board = []
-		for i in range(10):
-			board_row = []
-			for j in range(10):
-				board_row.append(-1)
-			board.append(board_row)
+        #generate user coordinates from the user and try to make move
+        #if move is a hit, check ship sunk and win condition
+        while(True):
+            x = random.randint(1,10)-1
+            y = random.randint(1,10)-1
+            res = self.make_move(board,x,y)
+            if res == "hit":
+                print "Hit at " + str(x+1) + "," + str(y+1)
+                self.check_sink(board,x,y)
+                board[x][y] = ('\x1b[0;32;40m' + 'X' + '\x1b[0m')
+                if self.check_win(board):
+                    return "WIN"
+            elif res == "miss":
+                print "Sorry, " + str(x+1) + "," + str(y+1) + " is a miss."
+                board[x][y] = ('\x1b[0;31;40m' + "*" + '\x1b[0m')
 
-		#setup board
-		user_board = copy.deepcopy(board)
+            if res != "try again":
 
-		#add ships as last element in the array
-		user_board.append(copy.deepcopy(ships))
+                return board
 
-		#ship placement
-		user_board = computer_place_ships(user_board,ships)
-		count = 0
-		#game main loop
-		print "Welcome to battleship, which game mode do you want to simulate?"
-		print "\n"
-		test_bool = True
-		while (test_bool):
-			game_mode = raw_input("\tRandom (r)\n\tEstimated Guess (e)\n\tQ Learning (q)\n")
-			if game_mode == "r" or game_mode == "e" or game_mode == "q":
-				test_bool = False
-			else:
-				print "Invalid input. Please only enter r, e, or q"
-				test_bool = True
+    def estimate_mode(self,board):
+    #generate user coordinates from the user and try to make move
+    #if move is a hit, check ship sunk and win condition
+        while(True):
+            for k in range(10):
+                for i in range(10):
+                    if board[i][k] == ('\x1b[0;32;40m' + 'X' + '\x1b[0m') and i != 9:
+                        res = self.make_move(board, i+1, k)
+                        if res == "hit":
+                            print "Hit at " + str(i+2) + "," + str(k+1)
+                            self.check_sink(board,i+1,k)
+                            board[i+1][k] = ('\x1b[0;32;40m' + 'X' + '\x1b[0m')
+                            if self.check_win(board):
+                                return "WIN"
+                            return board
+                        if res == "miss":
+                            print "Sorry, " + str(i+2) + "," + str(k+1) + " is a miss."
+                            board[i+1][k] = ('\x1b[0;31;40m' + "*" + '\x1b[0m')
+                            return board
+                        elif res == "try agian" and i!= 9:
+                            i = i + 1
+                            res = self.make_move(board, i, k)
+                            if res == "try again" and i!= 9:
+                                i = i + 1
+                                res = self.make_move(board, i, k)
 
-		if game_mode == 'r':
+            x = random.randint(1,10)-1
+            y = random.randint(1,10)-1
+            res = self.make_move(board,x,y)
+            if res == "hit":
+                print "Hit at " + str(x+1) + "," + str(y+1)
+                self.check_sink(board,x,y)
+                board[x][y] = ('\x1b[0;32;40m' + 'X' + '\x1b[0m')
+                if self.check_win(board):
+                    return "WIN"
+            elif res == "miss":
+                print "Sorry, " + str(x+1) + "," + str(y+1) + " is a miss."
+                board[x][y] = ('\x1b[0;31;40m' + "*" + '\x1b[0m')
+            if res != "try again":
+                return board
 
-			while(1):
+    def check_sink(self,board,x,y):
 
-			#computer move
-				user_board = random_mode(user_board)
-				count = count + 1
-			#check if computer move
-				if user_board == "WIN":
-					print "GAME OVER"
-					print "Move count :",count,"."
-					quit()
+        #figure out what ship was hit
+        if board[x][y] == "A":
+            ship = "Aircraft Carrier"
+        elif board[x][y] == "B":
+            ship = "Battleship"
+        elif board[x][y] == "S":
+            ship = "Submarine"
+        elif board[x][y] == "D":
+            ship = "Destroyer"
+        elif board[x][y] == "P":
+            ship = "Patrol Boat"
 
-			#display user board
-				print_board("u",user_board)
-				raw_input("To end computer turn hit ENTER")
+        #mark cell as hit and check if sunk
+        board[-1][ship] -= 1
+        if board[-1][ship] == 0:
+            print ship + " Sunk"
 
-		if game_mode == 'e':
-			print "mode has not been implemented"
-		if game_mode == 'q':
-			print "mode has not been implemented"
 
-	if __name__=="__main__":
-		main()
+    def check_win(self,board):
+
+        #simple for loop to check all cells in 2d board
+        #if any cell contains a char that is not a hit or a miss return false
+        for i in range(10):
+            for j in range(10):
+                if board[i][j] != -1 and board[i][j] != ('\x1b[0;31;40m' + "*" + '\x1b[0m') and board[i][j] != ('\x1b[0;32;40m' + 'X' + '\x1b[0m'):
+                    return False
+        return True
+
+def main():
+
+    agent = Agent()
+    #types of ships
+    ships = {"Aircraft Carrier":5,
+             "Battleship":4,
+             "Submarine":3,
+             "Destroyer":3,
+             "Patrol Boat":2}
+
+    #setup blank 10x10 board
+    board = []
+    for i in range(10):
+        board_row = []
+        for j in range(10):
+            board_row.append(-1)
+        board.append(board_row)
+
+    #setup board
+    user_board = copy.deepcopy(board)
+
+    #add ships as last element in the array
+    user_board.append(copy.deepcopy(ships))
+
+    #ship placement
+    user_board = agent.computer_place_ships(user_board,ships)
+    count = 0
+    #game main loop
+    print "Welcome to battleship, which game mode do you want to simulate?"
+    print "\n"
+    test_bool = True
+    while (test_bool):
+        game_mode = raw_input("\tRandom (r)\n\tEstimated Guess (e)\n\tQ Learning (q)\n")
+        if game_mode == "r" or game_mode == "e" or game_mode == "q":
+            test_bool = False
+        else:
+            print "Invalid input. Please only enter r, e, or q"
+            test_bool = True
+
+    if game_mode == 'r':
+
+        while(1):
+
+        #computer move
+            user_board = agent.random_mode(user_board)
+            count = count + 1
+        #check if computer move
+            if user_board == "WIN":
+                print "GAME OVER"
+                print "Move count :",count,"."
+                quit()
+
+        #display user board
+            agent.print_board("u",user_board)
+            raw_input("To end computer turn hit ENTER")
+
+    if game_mode == 'e':
+
+        while(1):
+
+            user_board =  agent.estimate_mode(user_board)
+            count = count + 1
+        #check if computer move
+            if user_board == "WIN":
+                print "GAME OVER"
+                print "Move count :",count,"."
+                quit()
+            agent.print_board("u",user_board)
+            raw_input("To end computer turn hit ENTER")
+
+    if game_mode == 'q':
+        print "mode has not been implemented"
+        user_board = qlearning_mode(user_board)
+        count = count + 1
+        #check if computer move
+        if user_board == "WIN":
+            print "GAME OVER"
+            print "Move count :",count,"."
+            quit()
+
+if __name__=="__main__":
+    main()
 
 
